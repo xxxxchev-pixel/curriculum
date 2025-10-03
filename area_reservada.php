0a0cd43c8d4277919d5ee82f9145f51ac0df4912
hhkhkhjk <xxxxchev@gmail.com>
2025-10-03 09:52:33 +0100

    7atualizacao

--- area_reservada.php
+++
@@ -1,8 +0,0 @@
-/* area_reservada.php */
-<?php
-session_start();
-if (!isset($_SESSION["autenticado"]) || $_SESSION["autenticado"] !== true) {
-    header("Location: login.php");
-    exit();
-}
-?>
--- logout.php
+++
@@ -1,7 +0,0 @@
-/* logout.php */
-<?php
-session_start();
-session_destroy();
-header("Location: login.php");
-exit();
-?>
